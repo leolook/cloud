f114b95e7d70dfe948d0dc66a344f6e832fcea10
hwt <xiongjiehu@126.com>
2018-05-03 21:09:48 +0800

    管理员登录初实现

---
+++ httpServer/controller/admin.go
@@ -0,0 +1,15 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type Admin struct{}
+
+//登录
+func (Admin) Login(c *gin.Context) {
+
+}
+
+//推出登录
+func (Admin) LoginOut(c *gin.Context) {
+
+}
