509e3db6987c07bdd2d11136562b20a146f59e20
xiongjiehu@126.com <xiongjiehu@126.com>
2018-05-03 23:35:06 +0800

    登录注册

--- httpServer/route.go
+++ httpServer/route.go
@@ -31,17 +31,23 @@ const (
 
 //请求url
 const (
-	HEAD        = "/cloud"
-	PING        = HEAD + "/ping"
-	UPLOAD_FILE = HEAD + "/uploadFile" //上传文件
-	DEL_FILE    = HEAD + "/delFile"    //文件删除
+	HEAD              = "/cloud"
+	ADMIN             = HEAD + "/admin"
+	PING              = HEAD + "/ping"
+	ADMIN_UPLOAD_FILE = ADMIN + "/uploadFile" //上传文件
+	ADMIN_DEL_FILE    = ADMIN + "/delFile"    //文件删除
+	ADMIN_LOGIN       = ADMIN + "/login"      //登录
+	ADMIN_LOGIN_OUT   = ADMIN + "／loginOut"   //推出登录
+
 )
 
 func init() {
 	HttpRoute = make(map[string]*Controller)
 	HttpRoute[PING] = addRoute(GET_REQUEST, controller.Network{}.Ping)
-	HttpRoute[UPLOAD_FILE] = addRoute(POST_REQUEST, controller.File{}.Upload)
-	HttpRoute[DEL_FILE] = addRoute(GET_REQUEST, controller.File{}.DelFile)
+	HttpRoute[ADMIN_UPLOAD_FILE] = addRoute(POST_REQUEST, controller.File{}.Upload)
+	HttpRoute[ADMIN_DEL_FILE] = addRoute(GET_REQUEST, controller.File{}.DelFile)
+	HttpRoute[ADMIN_LOGIN] = addRoute(POST_REQUEST, controller.Admin{}.Login)
+	HttpRoute[ADMIN_LOGIN_OUT] = addRoute(POST_REQUEST, controller.Admin{}.LoginOut)
 }
 
 //启动server
