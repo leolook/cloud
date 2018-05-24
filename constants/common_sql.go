e82e98754b6370453fe516c8ff29b5c8c0fbfe71
hwt <xiongjiehu@126.com>
2018-05-24 16:57:46 +0800

    文件上传增加字段createTime,视频列表默认根据id排序

--- constants/common_sql.go
+++ constants/common_sql.go
@@ -17,7 +17,7 @@ const (
 	ADMIN_VIDEO_FILE_INSERT_SQL      = "insert into " + TABLE_VIDEO_FILE + "(name,info,cover,classify_id,update_user,create_time,update_time) values(?,?,?,?,?,unix_timestamp(now()),unix_timestamp(now()))"
 	ADMIN_VIDEO_FILE_UPDATE_SQL      = "update " + TABLE_VIDEO_FILE + " set name=?,info=?,cover=?,classify_id=?,update_user=?,update_time=unix_timestamp(now()) where id=?"
 	ADMIN_VIDEO_FILE_LIKE_SQL        = "and name like '%s' "
-	ADMIN_VIDEO_FILE_LIST_SQL        = "select * from " + TABLE_VIDEO_FILE + " where 1=1  %s order by id limit ?,?"
+	ADMIN_VIDEO_FILE_LIST_SQL        = "select * from " + TABLE_VIDEO_FILE + " where 1=1  %s  order by id desc limit ?,?"
 	VIDEO_FILE_COUNT_SQL             = "select count(*) from " + TABLE_VIDEO_FILE + " where 1=1  %s "
 	ADMIN_VIDEO_PATH_GET_SQL         = "select * from " + TABLE_VIDEO_PATH + " where file_id=?"
 	ADMIN_VIDEO_PATH_INSERT_SQL      = "insert into " + TABLE_VIDEO_PATH + "(file_id,path,number,info,create_time) values(?,?,?,?,?)"
